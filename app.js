0480dcff64b5aae58bb7bbe913278098b9fe9e23
Noah Ispas <noahispas@gmail.com>
2016-05-12 16:42:36 +0200

    added error handling for unknown routes

--- app.js
+++ app.js
@@ -20,6 +20,21 @@ app.use(express.static(path.join(__dirname, 'views')));
 var routes = require('./bin/routes.js');
 app.use('/', routes);
 
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: err
+  });
+});
+
 //connect and seed if necessary
 mongo_url = helper.get_mongo_url();
 mongoose.connect(mongo_url);
